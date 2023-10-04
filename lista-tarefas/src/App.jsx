2a86e1e01ba5cd35d782453090b8e9fa12cd5938
Nickz <nicolasaraujo845@gmail.com>
2023-10-04 10:36:15 -0300

    back do 1.0 version

--- lista-tarefas/src/App.jsx
+++ lista-tarefas/src/App.jsx
@@ -1,53 +1,94 @@
-// import {useState} from "react";
+import {useState} from "react";
 import './App.css';
-// import Todo from "./components/Todo";
-// import TodoForm from "./components/TodoForm";
-// import Search from "./components/Search";
-// import Filter from "./components/Filter";
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-
-import Login from './components/Login'
-import Logout from "./components/Logout";
-import Registro from "./components/Registro";
-import ListTodo from "./components/ListTodo";
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
+import Search from "./components/Search";
+import Filter from "./components/Filter";
 
 function App() {
 
-  // const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
-  // const [todos, setTodos] = useState(savedTodos);
+  const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+  const [todos, setTodos] = useState(savedTodos);
 
-  // const [search, setSearch] = useState("");
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id:1,
+  //     text: "Criar funcionalidade X no sistema",
+  //     category: "Trabalho",
+  //     isCompleted: false,
+  //   },
+  //   {
+  //     id:2,
+  //     text: "Ir para a academia",
+  //     category: "Pessoal",
+  //     isCompleted: false,
+  //   },
+  //   {
+  //     id:3,
+  //     text: "Estudar React",
+  //     category: "Estudos",
+  //     isCompleted: false,
+  //   }
+  // ]);
 
-  // const [filter, setFilter] = useState("All");
-  // const [sort, setSort] = useState("Asc");
+  const [search, setSearch] = useState("");
 
-  // const addTodo = (text, category) => {
-  //   const newTodos = [...todos, { id: Math.floor(Math.random() * 10000), text, category, isCompleted: false }];
-  //   setTodos(newTodos);
-  //   localStorage.setItem('todos', JSON.stringify(newTodos));
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+
+  const addTodo = (text, category) => {
+    const newTodos = [...todos, { id: Math.floor(Math.random() * 10000), text, category, isCompleted: false }];
+    setTodos(newTodos);
+    localStorage.setItem('todos', JSON.stringify(newTodos));
+  };
+  
+  
+  // const addTodo = (text, category)=>{
+  //   const newTodos = [...todos,{
+  //     id: Math.floor(Math.random()*10000),
+  //     text,
+  //     category,
+  //     isCompleted: false,
+  //   },
+  // ];
+  // setTodos(newTodos)
   // };
+
+  const removeTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+    localStorage.setItem('todos', JSON.stringify(newTodos));
+  };
+  
+  const completeTodo = (id) => {
+    const newTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    );
+    setTodos(newTodos);
+    localStorage.setItem('todos', JSON.stringify(newTodos));
+  };
   
   
-  // const removeTodo = (id) => {
-  //   const newTodos = todos.filter((todo) => todo.id !== id);
-  //   setTodos(newTodos);
-  //   localStorage.setItem('todos', JSON.stringify(newTodos));
+
+  // const removeTodo = (id) =>{
+  //   const newTodos = [...todos]
+  //   const filteredTodos = newTodos.filter((todo) =>
+  //    todo.id !== id ? todo : null
+  //    );
+  //   setTodos(filteredTodos)
   // };
-  
+
+  //sugestao do chatgpt
   // const completeTodo = (id) => {
   //   const newTodos = todos.map((todo) =>
   //     todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
   //   );
   //   setTodos(newTodos);
-  //   localStorage.setItem('todos', JSON.stringify(newTodos));
   // };
-  
-  
+
   return (
     <div className="app">
-      {/* <h1>Lista de Tarefas</h1>
+      <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch}></Search>
       <Filter filter={filter} setFilter={setFilter} setSort={setSort}></Filter>
       <div className="todo-list">
@@ -70,18 +111,7 @@ function App() {
          <Todo todo={todo} key={todo.id} removeTodo={removeTodo} completeTodo={completeTodo}/>
         ))}
       </div>
-      <TodoForm addTodo={addTodo}/> */}
-      <div>
-        <Router>
-          <Routes>
-            <Route path="/Registro" element={<Registro/>}/>
-            <Route path="/ListTodo" element={<ListTodo/>}/>
-            <Route path="/" element={<Login/>}/>
-            <Route path="/Logout" element={<Logout/>}/>
-          </Routes>
-        </Router>
-        {/* <Registro></Registro> */}
-      </div>
+      <TodoForm addTodo={addTodo}/>
     </div>
   );
 }
